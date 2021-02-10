848ab473d57854b1a58c816a4f5b766c63dc983b
Bharat Kumar <Bharkumar8@gmail.com>
2021-02-09 17:51:05 -0800

    Added files and created cell xib

---
+++ SportEvents/SportEvents/Views/ContentCell.swift
@@ -0,0 +1,18 @@
+//
+//  ContentCell.swift
+//  SportEvents
+//
+//  Created by Bharat Kumar on 2/9/21.
+//
+
+import UIKit
+
+class ContentCell: UICollectionViewCell {
+    
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventTitle: UILabel!
+    @IBOutlet weak var eventLocation: UILabel!
+    @IBOutlet weak var eventDate: UILabel!
+    
+    
+}
