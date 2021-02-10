94aab0bd3ead0bd0f41e5e915ac76d560f1ef51c
Bharat Kumar <Bharkumar8@gmail.com>
2021-02-09 19:52:11 -0800

    Implemented Event model

--- SportEvents/Event.swift
+++ SportEvents/Event.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct EventResults: Codable, Equatable {
+    let results: [Event]
+}
+struct Event: Codable, Equatable {
+    let title: String
+    let type: String
+    let dateTimeLocal: String
+    let url: String
+    let performers: [Performers]
+    let stats: Stats
+    let venue: Venue
+    let id: Int
+}
+
+struct Performers: Codable, Equatable {
+    let name: String
+    let url: String
+}
+
+struct Stats: Codable, Equatable {
+    let listingCount: Int?
+    let lowestPrice: Int?
+    let highestPrice: Int?
+}
+
+struct Venue: Codable, Equatable {
+    let city: String
+    let name: String
+}
